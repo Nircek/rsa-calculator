40821ef5a9d44591181b71056eb49ba7e3c084e1
Nircek <nircek-2103@protonmail.com>
2020-07-03 18:07:14 +0200

    Add checking if the number is prime

--- site.js
+++ site.js
@@ -5,10 +5,18 @@ const ginit = () => {
     .forEach((e) => {
       g[e.id] = e;
     });
+  const random = () => {
+    g.number.value = randomNumber(g.bits.value);
+  };
+  const if_prime = () => {
+    let arr = [g.number_prime, g.number_not_prime];
+    if (bigInt(g.number.value).isProbablePrime()) arr.reverse();
+    arr[0].classList.add('hidden');
+    arr[1].classList.remove('hidden');
+  };
   Object.entries({
-    random: () => {
-      g.number.value = randomNumber(g.bits.value);
-    },
+    random: random,
+    if_prime: if_prime,
   }).forEach((e) => {
     g[e[0]].onclick = e[1];
   });
