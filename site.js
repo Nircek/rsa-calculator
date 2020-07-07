eaa326695de0d0ad57caa2a42aee4d99f1412c31
Nircek <nircek-2103@protonmail.com>
2020-07-07 20:06:07 +0200

    Add calculating k

--- site.js
+++ site.js
@@ -29,6 +29,29 @@ const ginit = () => {
   const calc_phi = () => {
     g.phi.value = bigInt(g.p.value).minus(1).times(bigInt(g.q.value).minus(1));
   };
+  const add_td = (tr, text) => {
+    let td = document.createElement('td');
+    td.innerText = text;
+    tr.appendChild(td);
+  };
+  const calc_k = () => {
+    let tbody = document.querySelectorAll('#k_table > tbody')[0];
+    tbody.innerHTML = '';
+    for (let k = 0, last = 0; k == 1 || last != 1; ++k) {
+      let tr = document.createElement('tr');
+      tbody.appendChild(tr);
+      add_td(tr, k);
+      last = bigInt(g.phi.value).times(k).plus(1);
+      add_td(tr, last);
+      last = last.mod(g.e.value);
+      add_td(tr, last);
+      if (last == 0) {
+        g.k.value = k;
+        return;
+      }
+    }
+    g.k.value = 'gcd(\u03c6(n),e)\u22601';
+  };
   Object.entries({
     random: random,
     if_prime: if_prime,
@@ -37,6 +60,7 @@ const ginit = () => {
     atoq: atoq,
     calc_n: calc_n,
     calc_phi: calc_phi,
+    calc_k: calc_k,
   }).forEach((e) => {
     g[e[0]].onclick = e[1];
   });
